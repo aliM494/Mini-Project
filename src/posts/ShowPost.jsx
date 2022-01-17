1b6207ccc6f0d8ada70f17c9601512d6655ba6a1
aliM494 <88322172+aliM494@users.noreply.github.com>
2022-01-17 20:55:05 +0330

    End coding show post Page

--- src/posts/ShowPost.jsx
+++ src/posts/ShowPost.jsx
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { jpAxios } from "../jpAxios";
+import { handelGetWithId } from "../services/Services";
 
 const ShowPost = () => {
   const { postId } = useParams();
   const [post, setPost] = useState({});
+  const [user, setUser] = useState({ userName: "", userId: 0 });
 
   useEffect(() => {
-    jpAxios.get(`/posts/${postId}`).then((res) => {
+    handelGetWithId("posts", postId).then((res) => {
       setPost(res.data);
     });
-  });
+  }, []);
+
+  useEffect(() => {
+    handelGetWithId("users", post.userId).then((res) => {
+      setUser({ userName: res.data.username, userId: res.data.id });
+    });
+  }, [post]);
 
   return (
     <>
       <div className="row justify-content-center mt-5 ">
-        <div class="card " style={{ width: "18rem" }}>
+        <div class="card" style={{ width: "40rem" }}>
           <div class="card-body">
-            <h5 class="card-title">{post.title}</h5>
+            <h3 class="card-title">{post.title}</h3>
             <p class="card-text">{post.body}</p>
+
+            <div className="col-12 text-start">
+              <p className="ms-2">UserName: {user.userName} </p>
+              <p> {user.userId} :UserID </p>
+            </div>
           </div>
         </div>
       </div>
